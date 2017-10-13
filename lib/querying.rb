e9e0d67892168d32dc11a6834a1355f87f7190f6
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-13 23:55:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,7 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year FROM series
   JOIN books ON books.series_id = series.id
-  ORDER BY books.year"
+  ORDER BY series.year, books.year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
