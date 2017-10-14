5a7e171ee7366b13d1a151daebbecf8e1a6bc096
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-14 00:02:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species) AS total_species FROM characters
-  GROUP BY species ORDER BY total_species DESC LIMIT 1"
+  GROUP BY species
+  ORDER BY total_species DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
