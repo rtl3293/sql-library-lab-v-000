13fc0b4a262a09578775f885acf714cde0b955ec
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-14 00:05:48 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM authors
+  JOIN series ON authors.id = series.author_id
+  JOIN subgenres ON series.subgenre_id = subgenre.id
+  GROUP BY authors.name"
 end
 
 def select_series_title_with_most_human_characters
