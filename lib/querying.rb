9f8d2b41b8d615892879cd028154c8e66744e381
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-14 00:08:42 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,11 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series
+  JOIN books ON series.id = books.series_id
+  JOIN characters ON books.series_id =characters.series_id
+  GROUP BY series.title
+  ORDER BY "
 end
 
 def select_character_names_and_number_of_books_they_are_in
